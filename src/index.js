dc207d1852fd046e7898cd79a1aca1a97df5dcfb
Haris Ali <haris.ali@empglabs.com>
2022-08-10 16:15:52 +0500

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { ConnectedApp, store, persistor } from "./App";
+import { Provider, connect } from "react-redux";
+
+import reportWebVitals from "./reportWebVitals";
+//index.js
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+//...
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <ConnectedApp />
+    </PersistGate>
+  </Provider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
