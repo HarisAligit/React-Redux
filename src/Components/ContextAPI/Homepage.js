bae285c13548c7ef7d127b7de6ff0f88286db12d
Haris Ali <haris.ali@empglabs.com>
2022-08-15 13:05:19 +0500

    Removed Errors

--- src/Components/ContextAPI/Homepage.js
+++
@@ -1,8 +0,0 @@
-import React, { useContext } from "react";
-import contextProvider from "./ThemeContext";
-
-export const HomePage = () => {
-  const colorValue = useContext(contextProvider);
-
-  return <div>{colorValue}</div>;
-};
