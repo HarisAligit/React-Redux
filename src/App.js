4b0f9e7e26e5b34c5647116553e845076bdda696
Haris Ali <haris.ali@empglabs.com>
2022-08-15 12:42:02 +0500

    Added Required Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from "react";
+import {useState} from "react";
+import {AppContext} from "./Context/context";
+import AddUser from "./Components/ContextAPI/AddUser";
+import UserList from "./Components/ContextAPI/UserList";
 
 const App = () => {
+  const [users, setUsers] = useState([]);
 
-    return (
-      <div>
-        {/*{Your Code Here}*/}
-      </div>
-    );
+  const dispatchUserEvent = (actionType, payload) => {
+    switch (actionType) {
+      case 'ADD_USER':
+        setUsers([...users, payload.newUser]);
+        return;
+      case 'REMOVE_USER':
+        setUsers(users.filter(user => user.id !== payload.userId));
+        return;
+      default:
+        return;
+    }
+  }
+
+  return (
+    <div>
+      <AppContext.Provider value={{users, dispatchUserEvent}}>
+        <AddUser />
+        <UserList />
+      </AppContext.Provider>
+    </div>
+  );
 }
 
 export default App;
